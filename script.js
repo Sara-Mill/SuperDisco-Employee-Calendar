6369c6a47c6d2d5dc274e5112f06f03d36d5f41a
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-08-17 10:42:33 -0600

    color blocks working

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 var currentDayEl = $("#currentDay");
-var timeBlokEl = $("#timeBlock");
+var timeBlockEl = $(".time-block");
 var now = [];
 var now = moment().format("dddd, MMM Do, YYYY hh:mm a");
+//console.log(now)
 var displayTime = []; 
+var hourEl = $("#hour");
 
 //Display time at top of the page
 currentDayEl.text(now);
-console.log(now);
+//get current time
+var currentTime = moment().format('h A');
+//console.log(currentTime);
 
 setInterval(displayTime, 1000); //updates time to the second
 
-function showTime() {
-  var objDate = newDate();
-    var hour = objDate.getHours();
-    if(hours >=9 && hours <=17) {
-      $(".hour").addClass("past");
-    } else {
-      $(".hour").addlass("future");
-    }
-    
-    //if rightNow < timeBlockEl("#id") {
-      //compare hour with id in class "time-block" in HTML
-    }
-  
-//}
+//if hour is < current time, I want the textarea to turn color
 
+//make the function for color change
+function timeTracker () {
+  var timeNow = moment().format('h');
+
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+    
+    if (blockTime < timeNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (blockTime > timeNow) {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    }
+  })
+}
+//call the function for color change
+timeTracker();
